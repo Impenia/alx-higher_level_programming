470a88b6d85ed0157e232846fbce8a5a584074c4
Impenia <akpaetuki@gmail.com>
2023-04-26 01:56:26 +0100

    who was playing in this movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,13 +1,30 @@
 #!/usr/bin/node
 // retrieves all character names in SW film
 const request = require('request');
-const FILM_URL = `http://swapi.co/api/films/${process.argv[2]}`;
-request(FILM_URL, function (error, response, body) {
+
+const movieID = process.argv[2];
+
+request(`https://swapi-api.alx-tools.com/api/films/${movieID}`, function (error, response, body) {
   if (error) {
-    throw new Error(error);
-  }
-  for (const url of JSON.parse(body).characters) {
-    request(url, (error, response, body) =>
-      !error && console.log(JSON.parse(body).name));
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    const characterPromises = movie.characters.map(characterUrl => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, function (error, response, body) {
+          if (error) {
+            reject(error);
+          } else {
+            const character = JSON.parse(body);
+            resolve(character.name);
+          }
+        });
+      });
+    });
+    Promise.all(characterPromises).then(characters => {
+      console.log(characters.join('\n'));
+    }).catch(error => {
+      console.error(error);
+    });
   }
 });
